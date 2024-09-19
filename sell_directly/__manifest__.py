cecd9485a2f2e4c2fbe21f8285cea6ab300df628
Sandraraouf2023 <s.sedik@t-petra.com>
2024-09-19 17:27:47 +0200

    version 15

---
+++ sell_directly/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name':'Sell Directly',
+    'summary':'Special Serial Number and sell directly from purchase',
+    'depends': ['base', 'product','sale','stock','web_tour','purchase','account'],
+    'category':'Sales',
+
+    'data':[
+       
+            'views/sales_orderline.xml',
+            'views/purchase_orderline.xml'  ,
+            'views/invoice_serial.xml'   
+
+            ],
+            'website': 'www.t-petra.com',
+    'author':'Petra Software',
+    'company': 'Petra Software',
+    'maintainer': 'Petra Software',
+    'images': ['static/description/banner.png'],
+        'price':15,
+      'currency':'USD', 
+      'license':'AGPL-3'   
+         
+        
+    }
